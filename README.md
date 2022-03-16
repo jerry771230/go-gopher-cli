1b613c10dd42533a04f894e3c0a5c6bd26b03167
Jerry Huang <jerry771230@hotmail.com>
2022-03-16 16:52:25 +0800

    feat: Add `get` command

--- README.md
+++ README.md
@@ -45,6 +45,47 @@ A `main.go` file and a `cmd` folder has been created like this:
 More details on using the `cobra-cli` generator, please read [The Cobra Generator README](https://github.com/spf13/cobra-cli/blob/main/README.md)
 More details on using the `cobra` library, please read [The Cobra User Guide](https://github.com/spf13/cobra/blob/master/user_guide.md)
 
+## Add first command
+
+Use `cobra-cli` to add new command.
+
+```bash
+$ cobra-cli add get
+```
+
+### Test it
+
+Test the help of `get` command,
+
+```bash
+$ go run ./main.go get -h
+This get command will call GitHub respository in order to return the desired Gopher.
+
+Usage:
+  go-gopher-cli get [flags]
+
+Flags:
+  -h, --help   help for get
+```
+
+Test get command,
+
+```bash
+$ go run ./main.go get friends
+Try to get 'friends' Gopher...
+Perfect! Just saved in friends.png!
+```
+
+Test with an unknown Gopher,
+
+```bash
+$ go run ./main.go get awesome
+Try to get 'awesome' Gopher...
+Error: awesome not exists! :-(
+```
+
+
+
 
 ## Ref
 
---
+++ cmd/get.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "This command will get the desired Gopher",
+	Long:  `This get command will call GitHub respository in order to return the desired Gopher.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var name = "dr-who.png"
+
+		if len(args) >= 1 && args[0] != "" {
+			name = args[0]
+		}
+
+		url := "https://github.com/scraly/gophers/raw/main/" + name + ".png"
+
+		fmt.Println("Try to get '" + name + "' Gopher...")
+
+		// Get the data
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode == 200 {
+			// Create the file
+			file, err := os.Create(name + ".png")
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer file.Close()
+
+			// Writer the body to file
+			_, err = io.Copy(file, response.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Perfect! Just saved in " + file.Name() + "!")
+		} else {
+			fmt.Println("Error: " + name + " not exists! :-(")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
